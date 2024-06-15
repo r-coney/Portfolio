416f88662645feb8693c3275f9816fafd9af6bf0
rstar <rstar0823@icloud.com>
2024-06-15 20:44:37 +0900

    change: 紛らわしかったため、fontの変数名を変更

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,9 +1,15 @@
 import Top from "@/components/Top/Top";
+import Skills from "@/components/Top/Skills";
 
 export default function Home() {
   return (
-  <div className="section-container">
-    <Top />
-  </div>
+    <>
+      <div className="section-container">
+        <Top />
+      </div>
+      <div>
+        <Skills />
+      </div>
+    </>
   );
 }
---
+++ app/src/components/top/Skills.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Skills = () => {
+  return (
+  <>
+  <h2 className='section_title'>Skills</h2>
+  </>
+  )
+}
+
+export default Skills
