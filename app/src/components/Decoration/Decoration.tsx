966c809f30e004bc287ae0f76a46055d4f284ff1
rstar <rstar0823@icloud.com>
2024-06-29 12:18:20 +0900

    feat: Decorationのアニメーションを表示された時に発火するよう変更

--- app/src/components/Decoration/Decoration.tsx
+++ app/src/components/Decoration/Decoration.tsx
@@ -1,21 +1,21 @@
 "use client";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "./Decoration.module.css";
 import { ReactTyped } from "../../../node_modules/react-typed/dist/mjs/index";
 
 const Decoration = () => {
   const codeString = `
 const splitMessage = [
-\t'T', 'h', 'a', 'n', 'k', 's', ' ', 
-\t'f', 'o', 'r', ' ', 
-\t'c', 'o', 'm', 'i', 'n', 'g', '!', 
-]; 
+\t'T', 'h', 'a', 'n', 'k', 's', ' ',
+\t'f', 'o', 'r', ' ',
+\t'c', 'o', 'm', 'i', 'n', 'g', '!',
+];
 
 
-let message = ''; 
-splitMessage.forEach(char => { 
-\tmessage += char; 
-}); 
+let message = '';
+splitMessage.forEach(char => {
+\tmessage += char;
+});
 
 
 console.log(message);
@@ -29,16 +29,41 @@ Thanks for coming!
 You have completely understood programming!
   `;
 
+  const ref = useRef(null);
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true);
+          observer.disconnect();
+        }
+      },
+      {
+        threshold: 0.1,
+      }
+    );
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => observer.disconnect();
+  }, []);
+
   return (
     <div className={styles.decoration}>
-      <div className={styles.decoration_text_container}>
+      <div className={styles.decoration_text_container} ref={ref}>
         <code className={styles.decoration_text}>
-          <ReactTyped
+        {isVisible && (
+           <ReactTyped
             strings={[codeString]}
             typeSpeed={40}
             backSpeed={50}
             loop={false}
           />
+        )}
         </code>
       </div>
     </div>
